8a2119c5e04e628484b7cea60ed658b043c2791e
barnabas666 <barnabas666@email.cz>
2025-01-12 18:14:48 +0100

    Add Post-Deployment Script and publish DB.

---
+++ StaffAttDB/Scripts/Script.PostDeployment.sql
@@ -0,0 +1,21 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+if not exists (select 1 from dbo.Departments)
+begin
+    insert into dbo.Departments(Title, Description)
+    values ('HR', 'Human resources department.'),
+    ('Management', 'Management department.'),
+    ('IT', 'IT department.'),
+    ('Marketing', 'Marketing department.'),
+    ('Accounting', 'Accounting department.');
+end
