9bdd340012246a1287435c6d90045abc0561b1f7
barnabas666 <barnabas666@email.cz>
2025-02-07 14:51:24 +0100

    Add StaffAtt.Desktop project, setup DI.
    Desktop app for executing Staff's CheckIns/Outs.

---
+++ StaffAtt.Desktop/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StaffAttLibrary.Data;
+using StaffAttLibrary.Db;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Windows;
+
+namespace StaffAtt.Desktop;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    public App()
+    {
+        ServiceCollection serviceCollection = new();
+        serviceCollection.ConfigureServices();
+
+        ServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+        var mainWindow = serviceProvider.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+    }
+}
+
+public static class ServiceCollectionExtensions
+{
+    public static void ConfigureServices(this IServiceCollection services)
+    {
+        IConfiguration configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appSettings.json", false)
+        .Build();
+
+        services.AddSingleton<IConfiguration>(configuration);
+
+        services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+        services.AddSingleton<IDatabaseData, SqlData>();
+        services.AddSingleton<MainWindow>();
+    }
+}
+
---
+++ StaffAtt.Desktop/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using StaffAttLibrary.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StaffAtt.Desktop;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private readonly IDatabaseData _sqlData;
+
+    public MainWindow(IDatabaseData sqlData)
+    {
+        InitializeComponent();
+        _sqlData = sqlData;
+    }
+}
